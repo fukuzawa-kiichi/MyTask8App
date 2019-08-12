3ea61241582e423684c0366de16d9a46f32e73f8
VERTEX24 <vertex24@VERTEX24noMacBook-Air.local>
2019-08-12 20:15:10 +0900

    first commit

---
+++ MyTask8App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTask8App
+//
+//  Created by VERTEX24 on 2019/08/12.
+//  Copyright © 2019 VERTEX24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
