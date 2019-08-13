a9fc64d0414d5c92279f170f3fb5384b73b64203
VERTEX24 <vertex24@VERTEX24noMacBook-Air.local>
2019-08-13 12:28:57 +0900

    数字の追加をできないようした

--- MyTask8App/ViewController.swift
+++ MyTask8App/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
     var operation = 0;
     // 最初の処理かどうか
     var isFirst = true
+    // 数字を追加させない
+    var notAddNum = true
 
     @IBOutlet weak var Label: UILabel!
     
@@ -36,21 +38,33 @@ class ViewController: UIViewController {
             Label.text! = ""
             // 一度だけ実行したい処理
         }
-        
-        if math == true {
-            Label.text = String(sender.tag - 1)
-            screenNum = Double(Label.text!)!
-            math = false
-
-        }else {
-            Label.text = Label.text! + String(sender.tag - 1)
-            screenNum = Double(Label.text!)!
+        if notAddNum {
+            // 2個目の数字のとき
+            if math == true {
+                Label.text = String(sender.tag - 1)
+                screenNum = Double(Label.text!)!
+                math = false
+                // はじめの数字のとき
+            }else {
+                Label.text = Label.text! + String(sender.tag - 1)
+                screenNum = Double(Label.text!)!
+            }
+        }else{
+            
         }
     }
     
-    // 演算子部分のボタアンが押されたときの処理
+    
+    // 演算子部分のボタンが押されたときの処理
     @IBAction func operators(_ sender: UIButton) {
-        if Label.text != "" && sender.tag != 11 && sender.tag != 16 {
+        if isFirst {
+            isFirst = true
+            Label.text! = ""
+            // 一度だけ実行したい処理
+        }
+ 
+        // +,-,×,÷が押されたとき
+        if Label.text != "" && sender.tag != 11 && sender.tag != 16 && operation == 0{
             previousNum = Double(Label.text!)!
             switch sender.tag {
             case 12:
@@ -63,7 +77,24 @@ class ViewController: UIViewController {
                 Label.text = "+";
             }
             operation = sender.tag
-            math = true;
+            math = true
+            notAddNum = true
+        // 演算子が2度押されたとき
+        }else if Label.text != "" && sender.tag != 11 && sender.tag != 16 && operation != 0{
+            switch sender.tag {
+            case 12:
+                Label.text = "÷";
+            case 13:
+                Label.text = "×";
+            case 14:
+                Label.text = "-";
+            default:
+                Label.text = "+";
+            }
+            operation = sender.tag
+            math = true
+            notAddNum = true
+         // =が押されたとき
         }else if sender.tag == 16 {
             switch operation {
             case 15:
@@ -75,11 +106,15 @@ class ViewController: UIViewController {
             default:
                 Label.text = String(previousNum / screenNum)
             }
+            operation = 0
+            notAddNum = false
+        // Cが押されたとき
         }else if sender.tag == 11 {
             previousNum = 0;
             screenNum = 0;
             operation = 0;
             Label.text = ""
+            notAddNum = true
         }
         
     }
